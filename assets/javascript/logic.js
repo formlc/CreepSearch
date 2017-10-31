fe3422668b99adde63954da7553720181d0f9f98
formlc <mcmahonhc@gmail.com>
2017-10-30 20:04:04 -0500

    update2

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	
+
+	 var config = {
+    apiKey: "AIzaSyAtIKkFHyiQ82mAH41vHZRdqVA_RNAPwWM",
+    authDomain: "creepsearch.firebaseapp.com",
+    databaseURL: "https://creepsearch.firebaseio.com",
+    projectId: "creepsearch",
+    storageBucket: "",
+    messagingSenderId: "724675415469"
+  	};
+  	
+  	firebase.initializeApp(config);
+
 })
