aab1e26a21b7b0eb4f7cb64a8cb480346e40a94f
formlc <mcmahonhc@gmail.com>
2017-11-04 11:01:51 -0500

    newnew

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
   	var database = firebase.database();
     var person = "";
     var alphaVanAPIkey = "FJH3LVLVBBGH5FWT";
+    var date = 
 
 
     $("#add-stock").on("click", function() {
@@ -27,19 +28,24 @@ $(document).ready(function() {
 
         var btnMaker = $("<button>")
 
-        btnMaker.addClass("btn")
-        btnMaker.addClass("stock-button")
-        btnMaker.html("<button>").text(person);
+        btnMaker.text(person);
+        btnMaker.addClass("btn");
+        btnMaker.addClass("stock-button");
 
-        $("button-area").append(btnMaker);
+        console.log(btnMaker);
+        console.log($(btnMaker));
+
+        $(".button-area").append(btnMaker);
+
+        console.log(response);
+        var open = response["Time Series (Daily)"]["2017-06-15"]["1. open"];
 
         TESTER = document.getElementById('result-panel-left');
         Plotly.plot( TESTER, [{
         x: [1, 2, 3, 4, 5],
         y: [1, 2, 4, 8, 16] }], {
         margin: { t: 0 } } );
-
-        console.log(response); 
+ 
 
       }).fail(function(error) {
         console.log(error);
